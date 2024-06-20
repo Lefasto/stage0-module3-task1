a025a0dbd5460434ad52fb5719b0d94636671ed2
Lefasto <147916190+Lefasto@users.noreply.github.com>
2024-06-20 20:01:32 +0200

    make it better

--- src/main/java/school/mjc/stage0/module3/task1/PrintWordsApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/PrintWordsApp.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.module3.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.println("Java");
+        System.out.println("java");
         System.out.println("is");
         System.out.println("smth");
     }
